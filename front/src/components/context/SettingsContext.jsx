f96c4accc36f5d851fd4dfa8b83bed9439ce3df5
uo293758 <uo293758@uniovi.es>
2026-05-02 22:23:13 +0200

    fix: Default font setting wasn't working

--- front/src/components/context/SettingsContext.jsx
+++ front/src/components/context/SettingsContext.jsx
@@ -62,6 +62,8 @@ export const SettingsProvider = ({ children }) => {
     }, [updateZoom]);
 
     const value = {
+        defaultFont,
+        updateDefaultFont,
         zoomIndex,
         setZoomIndex,
         updateZoom,
