6432b0f49b5e13b66ed5de7d835045a2508cb27c
uo293758 <uo293758@uniovi.es>
2026-05-16 18:28:58 +0200

    test: Configured WebDriverIO with a simple example test

---
+++ front/wdio.conf.js
@@ -0,0 +1,39 @@
+import path from 'path';
+
+// Detectamos si estamos en Windows para añadir el ".exe" al binario
+const isWindows = process.platform === 'win32';
+const binaryName = isWindows ? 'graphite.exe' : 'graphite';
+
+export const config = {
+    // Where the test specs are located
+    specs: [
+        './tests-e2e/**/*.spec.js'
+    ],
+    maxInstances: 1, // Compulsory for desktop apps
+
+    // App capabilities
+    capabilities: [{
+        maxInstances: 1,
+        browserName: 'wry',
+        'tauri:options': {
+            application: path.join(process.cwd(), `../tauri/target/debug/${binaryName}`)
+        }
+    }],
+
+    // Environment and test configurations
+    logLevel: 'error',
+    bail: 0,
+    baseUrl: 'http://localhost',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+
+    // We use the local runner and the Mocha framework (simple and straightforward)
+    services: [], // We will manage the tauri-driver through scripts to avoid port issues
+    framework: 'mocha',
+    reporters: ['spec'],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    }
+};
